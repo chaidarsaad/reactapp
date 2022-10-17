8b40bd85de4e7c483938b64ef370cd31d122b376
Chaidar Saad <chaidarsaad55@gmail.com>
2022-10-17 15:46:48 +0700

    rudi

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World &#128526;
+          Hello Rudi &#128526;
         </p>
         <a
           className="App-link"
